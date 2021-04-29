b4dfb372a14beb0f0951586d2c51d5d1fe4298c8
80502 <dh.kim@lgcns.com>
2021-04-29 10:11:05 +0900

    mav setting

--- src/main/java/com/daradat/exam/dp/controller/MainController.java
+++ src/main/java/com/daradat/exam/dp/controller/MainController.java
@@ -3,7 +3,10 @@ package com.daradat.exam.dp.controller;
 import com.daradat.exam.cm.dto.Exam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +19,24 @@ public class MainController {
     private Exam exam;
 
     @RequestMapping("index")
-    public String indexView(HttpServletRequest request){
-        System.out.println(exam);
-        return "index";}
+    public ModelAndView indexView(HttpServletRequest request){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("index");
+        mv.addObject("a", exam);
+
+        return mv;
+    }
+//
+//    @GetMapping("detail/{boardId}")
+//    public ModelAndView detail(@PathVariable int boardId){
+//        ModelAndView mv = new ModelAndView();
+//        BoardDto boardDto = boardService.searchById(boardId);
+//        boardService.updateViewCnt(boardId);
+//
+//        mv.setViewName("detail");
+//        mv.addObject("details", boardDto);
+//
+//        return mv;
+//    }
 }
